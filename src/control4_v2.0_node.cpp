4ed63a86eb87d2d9887ede87d687dd4d1194a07f
YUKICHI6105 <kou.zawa.f@gmail.com>
2023-02-15 18:23:25 +0900

    initの前にNodeHandleをおかないように処理

--- src/control4_v2.0_node.cpp
+++ src/control4_v2.0_node.cpp
@@ -4,15 +4,30 @@
 #include "can_utils.hpp"
 #include<math.h>
 
-ros::NodeHandle n;
+void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg);
 
-ros::Publisher chatter = n.advertise<can_plugins::Frame>("can_tx",1000);
+ros::Publisher* chatter; 
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "controler4_ver2.0");
+
+  ros::NodeHandle n;
+
+  chatter = &(n.advertise<can_plugins::Frame>("can_tx",1000));
+
+  ros::Subscriber sub = n.subscribe("Joy", 1000, chatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
 
 void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
 { 
   if(msg->buttons[1]==1)
   {
-    chatter.publish(get_frame(0x100,static_cast<uint8_t>(3)));
+    chatter->publish(get_frame(0x100,static_cast<uint8_t>(3)));
   }
   //↑mode_velへ移行
 
@@ -30,21 +45,10 @@ void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
     r =-1.0f;
   }
   //右回転
-  chatter.publish(get_frame(0x101, y-x+r/2));
-  chatter.publish(get_frame(0x111, x+y+r/2));
-  chatter.publish(get_frame(0x121, x-y+r/2));
-  chatter.publish(get_frame(0x131, -x-y+r/2));
+  chatter->publish(get_frame(0x101, y-x+r/2));
+  chatter->publish(get_frame(0x111, x+y+r/2));
+  chatter->publish(get_frame(0x121, x-y+r/2));
+  chatter->publish(get_frame(0x131, -x-y+r/2));
   //chatter.publish(get_frame(0x101, x/static_cast<float>(sqrt(2))-y/static_cast<float>(sqrt(2))));
   //100右上、110左上、120左下、130右下
-}
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "controler4_ver2.0");
-  
-  ros::Subscriber sub = n.subscribe("Joy", 1000, chatterCallback);
-
-  ros::spin();
-
-  return 0;
 }
