eccc778194aa9c45b6acabdad1f1ab1850b739a6
YUKICHI6105 <kou.zawa.f@gmail.com>
2023-02-17 17:10:26 +0900

    速度調整

--- src/control3_node.cpp
+++ src/control3_node.cpp
@@ -65,9 +65,9 @@ void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
     r =0.0f;
   }
   //右回転
-  chatter->publish(get_frame(0x101, 2*x+r/2));
-  chatter->publish(get_frame(0x111, x-y*static_cast<float>(sqrt(3))+r/2));
-  chatter->publish(get_frame(0x121, x-y*static_cast<float>(sqrt(3))+r/2));
+  chatter->publish(get_frame(0x101, 6.28f*(2*x+r)));
+  chatter->publish(get_frame(0x111, 6.28f*(x-y*static_cast<float>(sqrt(3))+r)));
+  chatter->publish(get_frame(0x121, 6.28f*(x-y*static_cast<float>(sqrt(3))+r)));
   //chatter.publish(get_frame(0x101, x/static_cast<float>(sqrt(2))-y/static_cast<float>(sqrt(2))));
   //100上、110左下、120右下
 }
